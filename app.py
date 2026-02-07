6b03e3c4d38324d3dcf26564cba41e4d790d7805
Wander Grevink <wander.grevink@gmail.com>
2026-02-07 11:57:12 +0100

    new release

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello! Version 0.0.6 of the application deployment is working! 🎉"
+    return "Hello! Version 0.0.7 of the application deployment is working! 🎉"
 
 
 if __name__ == "__main__":
